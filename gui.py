3da3c5a590b6ab0667d92f4843dc27e13bab1056
Dustin Armstrong <dustinarmstark@Dustins-Mac-mini.local>
2015-11-22 00:48:20 -0500

    added simple GUI

---
+++ gui.py
@@ -0,0 +1,35 @@
+import Tkinter
+import tkMessageBox
+
+window = Tkinter.Tk()
+window.title("The Slightly Annoying Prog (Beta)")
+window.geometry("350x250")
+
+def scripts():
+    #tkMessageBox.showinfo("Hello Python", "Hello World!")
+    import loop_scripts
+
+button = Tkinter.Button(window, text = "Click here to start", command = scripts)
+
+button.pack()
+window.mainloop()
+
+
+
+"""
+from Tkinter import *
+import tkMessageBox
+
+# creates window
+window = Tk()
+
+# modify window
+window.title("Test title")
+window.geometry("1000x500")
+
+button1 = Tkinter.Button(top, text="Button1", command = "loop_scripts.py")
+button1.pack()
+
+# event loop
+window.mainloop()
+"""
